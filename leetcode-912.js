617c906ddaf8d85ddf49976f95b5acb1475abf5f
Lucy <hanlucy12123@163.com>
2024-10-16 01:36:47 +0800

    Update leetcode-912.js

--- leetcode-912.js
+++ leetcode-912.js
@@ -1,8 +1,44 @@
 class Solution_912_SortAnArray {
     sortArray(nums) {
-        return this.quickSort(nums);
+        return this.mergeSort(nums);
     }
 
+    // Merge Sort
+    mergeSort(nums) {
+        if (nums.length <= 1) {
+            return nums;
+        }
+        const mid = Math.floor(nums.length / 2);
+        const left = nums.slice(0, mid);
+        const right = nums.slice(mid);
+        return this.merge(this.mergeSort(left), this.mergeSort(right));
+    }
+
+    merge(left, right) {
+        let i = 0;
+        let j = 0;
+        let result = [];
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                result.push(left[i]);
+                i++;
+            } else {
+                result.push(right[j]);
+                j++;
+            }
+        }
+        while (i < left.length) {
+            result.push(left[i]);
+            i++;
+        }
+        while (j < right.length) {
+            result.push(right[j]);
+            j++;
+        }
+        return result;
+    }
+
+    // Quick Sort
     quickSort(nums) {
         if (nums.length <= 1) {
             return nums;
@@ -14,14 +50,14 @@ class Solution_912_SortAnArray {
 
     _quickSort(nums, left, right) {
         if (left < right) {
-            let pivot = this._partition(nums, left, right);
+            const pivot = this._partition(nums, left, right);
             this._quickSort(nums, left, pivot - 1);
             this._quickSort(nums, pivot + 1, right);
         }
     }
 
     _partition(nums, left, right) {
-        let pivot = nums[right];
+        const pivot = nums[right];
         let i = left;
         for (let j = left; j < right; j++) {
             if (nums[j] < pivot) {
