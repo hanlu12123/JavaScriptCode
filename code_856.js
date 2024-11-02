f1448eea200da476fc4faf20f364f6b93ceeef7d
Lucy <hanlucy12123@163.com>
2024-11-03 03:28:48 +0800

    Update code_856.js

--- code_856.js
+++ code_856.js
@@ -1,1 +1,31 @@
-todo
+function scoreOfParentheses(s) {
+  const stack = [];
+  for (let c of s) {
+    if (c === '(') {
+      stack.push(c);
+    } else if (c === ')') {
+      let sum = 0;
+      while (stack.length > 0) {
+        let last = stack.pop();
+        if (last === '(') {
+          break;
+        } else if (typeof last === 'number') {
+          sum += last;
+        }
+      }
+      if (sum === 0) {
+        stack.push(1);
+      } else {
+        stack.push(sum * 2);
+      }
+    }
+  }
+  let res = 0;
+  for (let sItem of stack) {
+    if (typeof sItem === 'number') {
+      res += sItem;
+    }
+  }
+  return res;
+}
+
